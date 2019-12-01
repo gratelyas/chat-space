aa2608a57162506d86d525014a40286ea1fb3c52
gratelyas <gratel.yas@gmail.com>
2019-12-01 11:48:02 +0900

    edit-ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,61 +1,28 @@
-// $(function(){
-//   $('.new_message').on('submit', function(e){
-//     e.preventDefault()
-//     var formData = new FormData(this);
-//     var url = $(this).attr('action')
-//     $.ajax({
-//       url: url,
-//       type: "POST",
-//       data: formData,
-//       dataType: 'json',
-//       processData: false,
-//       contentType: false
-//     })
-//   })
-// })
-
-
-
 $(function(){ 
   function buildHTML(message){
-   if ( message.image ) {
-     var html =
-      `<div class="message" data-message-id=${message.id}>
-         <div class="upper-message">
-           <div class="upper-message__user-name">
-             ${message.user_name}
-           </div>
-           <div class="upper-message__date">
-             ${message.date}
-           </div>
-         </div>
-         <div class="lower-message">
-           <p class="lower-message__content">
-             ${message.content}
-           </p>
-         </div>
-         <img src=${message.image} >
-       </div>`
-     return html;
+   if (message.image) { 
+     var image = `<img src=${message.image}>`
    } else {
-     var html =
-      `<div class="message" data-message-id=${message.id}>
-         <div class="upper-message">
-           <div class="upper-message__user-name">
-             ${message.user_name}
-           </div>
-           <div class="upper-message__date">
-             ${message.date}
-           </div>
-         </div>
-         <div class="lower-message">
-           <p class="lower-message__content">
-             ${message.content}
-           </p>
-         </div>
-       </div>`
-     return html;
-   };
+     var image = ''
+   }
+  var html =
+    `<div class="message" data-message-id=${message.id}>
+      <div class="upper-message">
+        <div class="upper-message__user-name">
+          ${message.user_name}
+        </div>
+        <div class="upper-message__date">
+          ${message.date}
+        </div>
+      </div>
+      <div class="lower-message">
+        <p class="lower-message__content">
+          ${message.content}
+        </p>
+      </div>
+      ${image} 
+    </div>`
+  return html;
  }
 $('.new_message').on('submit', function(e){
  e.preventDefault();
