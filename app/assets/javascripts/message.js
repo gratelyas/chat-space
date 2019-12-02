6a192a3dc1881b9dc824efb3e33e34cee0920a1d
gratelyas <gratel.yas@gmail.com>
2019-12-02 12:07:44 +0900

    edit-3

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,21 +3,22 @@ $(function(){
     var image = (message.image)? `<img src=${message.image}>`: '';
     var html =
       `<div class="message" data-message-id=${message.id}>
-        <div class="upper-message">
-          <div class="upper-message__user-name">
-            ${message.user_name}
+        <div class="chat-main_message">
+          <div class="chat-main_message__upper-info">
+            <div class="chat-main_message__upper-info__talker">
+              ${message.user_name}
+            </div>
+            <div class="chat-main_message__upper-info__date">
+              ${message.date}
+            </div>
           </div>
-          <div class="upper-message__date">
-            ${message.date}
+          <div class="lower-message">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
           </div>
-        </div>
-        <div class="lower-message">
-          <p class="lower-message__content">
-            ${message.content}
-          </p>
-        </div>
-        ${image} 
-      </div>`
+          ${image} 
+        </div>`
     return html;
  }
 $('.new_message').on('submit', function(e){
